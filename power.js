9c75074801b3b602b315076857442af98b0005e1
Wiley Walker <wiley-walker@live.com>
2017-09-04 11:18:54 -0500

    added some stuff

--- power.js
+++ power.js
@@ -7,6 +7,7 @@ var constitution = 0;
 var health = 0;
 var magic = 0;
 
+		// Determine Character Abilities
 var app = angular.module('thisApp', []);
 app.controller('myCtrl', function($scope) {
      
@@ -17,7 +18,55 @@ app.controller('myCtrl', function($scope) {
         $scope.dexterity = randomNumber();
         $scope.charisma = randomNumber();
         $scope.constitution = randomNumber();
-    }
+		 
+
+		// Determine which characters are playable
+
+		if ($scope.strength < 12) {
+			  
+			 document.getElementById("warrior-button").disabled = true;
+		}
+		else{
+			 
+			document.getElementById("warrior-button").disabled = false;
+		}
+
+		if ($scope.intelligence <12){
+
+			document.getElementById("wizard-button").disabled = true;
+		}
+		else {
+			document.getElementById("wizard-button").disabled = false;
+		}
+
+		if ($scope.dexterity <12) {
+			document.getElementById("thief-button").disabled = true;
+		}
+		else {
+			document.getElementById("thief-button").disabled = false;
+		}
+
+		if ($scope.charisma && $scope.wisdom < 12) {
+			document.getElementById("bard-button").disabled = true;
+		}
+		else {
+			document.getElementById("bard-button").disabled = false;
+		}
+
+    	
+    	if ($scope.constituton < 12 || $scope.intelligence < 12) {
+    		document.getElementById("healer-button").disabled = true;
+    	}
+    	else {
+    		document.getElementById("healer-button").disabled = false;
+    	}
+    	if ($scope.strength < 13 || $scope.constituton < 13 || $scope.wisdom < 13) {
+    		document.getElementById("dragon-button").disabled = true;
+    	}
+    	else {
+    		document.getElementById("dragon-button").disabled = false;
+    	}
+}
          
 });
 
@@ -26,7 +75,9 @@ function randomNumber()  {
 	return num;
 }
 
-	console.log(randomNumber());
+ 
+
+	 
 
 function Ability() {
 	strength = randomNumber();
@@ -41,6 +92,8 @@ function Ability() {
 	return dexterity;
 	return charisma;
 	return constituton; 
-
+console.log("strength = " + strength);
 }
+
+ 
 
